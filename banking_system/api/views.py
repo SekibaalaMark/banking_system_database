6743be5e6432083d6f0901e99d9f0c62fdd168db
sekibaalamark <markmajje256@gmail.com>
2025-10-06 17:20:36 +0300

    created an api for adding branch

--- banking_system/api/views.py
+++ banking_system/api/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.db import connection
+import json
+
+@csrf_exempt
+def add_branch(request):
+    if request.method == "POST":
+        # Check if it's JSON or form-data
+        if request.content_type == 'application/json':
+            data = json.loads(request.body)
+        else:
+            data = request.POST
+            
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO Branch (branch_name, Location, phone_contact, manager_id)
+                VALUES (%s, %s, %s, %s)
+            """, [
+                data['branch_name'], 
+                data['location'], 
+                data['phone_contact'], 
+                data['manager_id']
+            ])
+        return JsonResponse({"message": "Branch added"})
+    
+
+    
