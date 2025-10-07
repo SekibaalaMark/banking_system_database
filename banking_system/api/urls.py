a2eb667ff79bd6420cfeec892f4f9c4f270cbc43
sekibaalamark <markmajje256@gmail.com>
2025-10-07 09:15:15 +0300

    registered the transaction api

--- banking_system/api/urls.py
+++ banking_system/api/urls.py
@@ -5,4 +5,8 @@ urlpatterns=[
     path('add_branch/',views.add_branch),
     path('add_employee/',views.add_employee),
     path('add_account/',views.add_account),
+    path('add_customer/',views.add_customer),
+    path('make_transaction/',views.make_transaction),
+    
+
 ]
